cbf990ad7df539f739843dd6088cc73fb09c9761
Somya Sarathi Samal <soms9edu@gmail.com>
2022-01-24 20:46:04 +0530

    Solved problem merge two array

---
+++ 01-array/11_merger2-arr.py
@@ -0,0 +1,30 @@
+import math
+import time
+
+
+def mergeArray(a: list[int], b: list[int], n: int, m: int) -> None:
+    i = n-1
+    j = 0
+    while i>=0 and j<m:
+        if a[i] > b[j]:
+            a[i], b[j] == b[j], a[i]
+        i -= 1
+        j += 1
+
+    return None
+
+
+if __name__ == '__main__':
+    START_TIME = time.time()
+
+    # input
+    n, m = [ int(x) for x in input().rstrip().split() ]
+    a = [ int(x) for x in input().rstrip().split() ]
+    b = [ int(x) for x in input().rstrip().split() ]
+    mergeArray(a, b, n, m)
+
+    print(a)
+    print(b)
+
+    print(f'program executed in {time.time() - START_TIME} second(s)')
+    exit(0)
